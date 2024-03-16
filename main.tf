a3e0276b106db7bd18ce51bd218bafb60fbc3280
saurabh71669 <71076639+saurabh71669@users.noreply.github.com>
2024-03-16 21:44:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket2024"
+resource "aws_s3_bucket" "this" {
+  bucket = "my-tf-test-bucket20242024"
 
   tags = {
     Name        = "My bucket-01"
